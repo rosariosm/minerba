502e2b78db25fc2e975709b54e24bf224b536539
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2018-08-10 21:00:20 -0300

    Add logo and processes images

--- js/text-rotator.js
+++ js/text-rotator.js
@@ -6,7 +6,7 @@ var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
+        this.period = parseInt(period) || 2000;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
@@ -27,7 +27,7 @@ var TxtType = function(el, toRotate, period) {
         var that = this;
         var delta = 200 - Math.random() * 100;
 
-        if (this.isDeleting) { delta /= 2; }
+        if (this.isDeleting) { delta /= 4; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
@@ -35,7 +35,7 @@ var TxtType = function(el, toRotate, period) {
         } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
-        delta = 500;
+        delta = 1000;
         }
 
         setTimeout(function() {
