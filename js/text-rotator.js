afc2d395dd0f6902adc6a7d552a480525527704b
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2018-08-18 22:47:45 -0300

    Add typewriter js lybrary

--- js/text-rotator.js
+++ js/text-rotator.js
@@ -1,60 +1,16 @@
 
-/* TYPEWRITER*/
 
 
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+$( document ).ready(function() {
+  
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+  var typed = new Typed('#typed', {
+    strings: ["POSICIONAR EMPRESAS Y PERSONAS", "ANTICIPAR, ADMINISTRAR Y SUPERAR CRISIS", "COMUNICAR DE MANERA EFECTIVA"],
+    typeSpeed: 60,
+    backSpeed: 12,
+    backDelay: 1300,
+    loop: true,
+    loopCount: Infinity,
+  });
+});
 
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = "<span class='wrap' style='border-bottom: 2px solid rgb(21, 132, 203); display: table-cell;'>"+this.txt+'</span><span class="cursor white-border" style="display: table-cell; font-weight: 100">|</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 4; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 1000;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        //var css = document.createElement("style");
-        //css.type = "text/css";
-        //css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        //document.body.appendChild(css);
-    };
