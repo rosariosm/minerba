061efc7657fdb71dbc3d216b2afcdcff0266434f
Rosario Santa Marina <rosariosantamarina1@gmail.com>
2018-09-23 21:01:36 -0300

    Fix text-rotator

--- js/text-rotator.js
+++ js/text-rotator.js
@@ -1,11 +1,26 @@
-$( document ).ready(function() {
-  var typed = new Typed('#typed', {
-    strings: ["POSICIONAR EMPRESAS Y PERSONAS", "ANTICIPAR, ADMINISTRAR Y SUPERAR CRISIS", "COMUNICAR DE MANERA EFECTIVA"],
-    typeSpeed: 60,
-    backSpeed: 12,
-    backDelay: 1300,
-    loop: true,
-    loopCount: Infinity,
+var typed;
+
+$( document ).ready(function() { 
+  window.typed = new Typed('#typed', {
+    strings: [""]    
   });
+  
 });
 
+$( window ).on( "load", function() { 
+
+        setTimeout(function() {
+          
+         window.typed.destroy();
+           var typed = new Typed('#typed', {
+            strings: ["POSICIONAR EMPRESAS Y PERSONAS", "ANTICIPAR, ADMINISTRAR Y SUPERAR CRISIS", "COMUNICAR DE MANERA EFECTIVA"],
+            typeSpeed: 60,
+            backSpeed: 12,    
+            backDelay: 1500,
+            loop: true,
+            loopCount: Infinity,
+          }); 
+            //your code to be executed after 1 second
+        }, 900);
+        
+});
